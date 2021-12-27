9f7136dc4638a9f1bff8a2f9800693f2bb698ba9
gabriel <xeneisegaby@gmail.com>
2021-12-27 05:43:48 -0300

    add drag and drop functionality

--- public/script.js
+++ public/script.js
@@ -1,50 +1,33 @@
-/*
-<div data-occupied="false" class="player-list draggable-item" >
-
-    <% players.forEach(player=>{ %>
-
-
-    
-      <div data-occupied="false" class="player">
-        <span data-occupied="false" class="player-ranking"><%=player.ranking%></span>.
-        <img src="<%=player.flag%>" class="flag"> 
-        <span data-occupied="false" class="player-name"><%=player.name%></span>
-      </div>
-    
-
-    <%});%>
-
-</div>
-*/
-
 let players = document.querySelectorAll('.player');
+let playerCells = document.querySelectorAll('.player-cell');
 let playerList = document.querySelector('.player-list');
+let playersArray = Array.from(players);
+let playerAux,endIndex,startIndex,dragOrigin,dragAllowed;
 
 players.forEach(player=>{
   player.addEventListener('dragover',dragOver);
   player.addEventListener('dragenter',dragOver);
   player.addEventListener('dragleave',dragLeave);
   player.addEventListener('dragstart',dragStart); 
-  player.addEventListener('drop',dragDropMenu);
-})
+  player.addEventListener('drop',dragDrop);
+});
 
+playerCells.forEach(cell=>{
+  cell.addEventListener('dragover',dragOver);
+  cell.addEventListener('dragenter',dragOver);
+  cell.addEventListener('dragleave',dragLeave);
+  cell.addEventListener('drop',dropCell);
+});
 
-
-let playersArray = Array.from(players);
-
-
-playersArray.forEach(player=>{
-  player.addEventListener('click',f)
-})
-
-function f(){
-  let i = playersArray.indexOf(this);
-  console.log(playersArray[i])
-}
-
-let playerAux,endIndex,startIndex;
 
 function dragStart(e){
+  
+  if(this.parentNode.className == 'player-list'){
+      dragOrigin = 'list';
+  }
+  else if(this.parentNode.classList.contains('player-cell')){
+    dragOrigin = 'cell';
+  }
   playerAux = this;
   startIndex = playersArray.indexOf(this);
 }
@@ -52,57 +35,73 @@ function dragStart(e){
 
 function dragOver(e){
   e.preventDefault();
-  this.style['background-color'] = "#009345";
+  // console.log(this,e.target)
+  if(dragOrigin === 'list' && e.target.parentNode.classList.contains('player-cell') ){
+    dragAllowed = false;
+  }
+  else{
+    this.style['background-color'] = "#009345";
+    endIndex = playersArray.indexOf(e.target);
+    dragAllowed = true;
+  }
 }
 
 function dragLeave(e){
   e.preventDefault();
     setTimeout(()=>{
       this.style['background-color'] = "";
-    },20)
+    },10);
 }
 
-function dragDropMenu(e){
+function dragDrop(e){
   e.preventDefault();
-  this.style['background-color'] = "";
-  endIndex = playersArray.indexOf(this);
+  // console.log(dragOrigin,dragAllowed);
+  // console.log(this.parentNode.classList.contains('player-cell'),e.target.parentNode);
 
-  playersArray.splice(startIndex,1,this)
-  playersArray.splice(endIndex,1,playerAux)
-  playerList.innerHTML = '';
+  if(dragAllowed){
+
+    if(dragOrigin === "list"){
+      endIndex = playersArray.indexOf(this);
+      playersArray.splice(startIndex,1,this)
+      playersArray.splice(endIndex,1,playerAux);
+    }
+    else if(dragOrigin === "cell" && !this.parentNode.classList.contains('player-cell')){
+      addPlayerToList(playerAux,endIndex);
+    }
+
+    this.style['background-color'] = "";
+    renderList();
+  }
+}
+
+
+function dropCell(e){
+  e.preventDefault(); 
+  
+  if(dragAllowed){
+    this.style['background-color'] = '';
+    this.append(playerAux);
+    removePlayerFromList(playerAux);
+    renderList();
+  } 
+}
+
+
+function addPlayerToList(player,index){
+  playersArray.splice(index,0,player)
+}
+
+
+function removePlayerFromList(player){
+  playersArray.splice(startIndex,1)
+}
+
+
+function renderList(){
+  playerList.replaceChildren();
 
   for(let i=0;i<playersArray.length;i++){
     playerList.append(playersArray[i])
   }
 }
-
-/*
-
-pregunta: para poder soltar un elemento en un div, el mismo tiene que tener draggable=true ?
-
-SOLTAR ELEMENTOS EN EL CUADRO:
-
-si arrastro el player sobre un gap: cambiar color del fondo del gap
-
-si suelto en ese gap: 
-  -append el jugador el gap
-  -elimino el jugador del array y renderizo denuevo la lista. va a estar sin ese player 
-
-si lo suelto en cualquier lado no pasa nada (vuelve a la lista)
-
--------
-
-si arrastro (drag over)un player sobre otro player Y el origen (drop start?) es un player que estaba en un gap (esto en la funcion dragover que ya esta (me fijo si la clase del this contiene draw gap)) entonces: 
-
-  -array.splice(posicion en que se solto,0 [porque no hay que borrar], player arrastrado)  .Se va a agregar debajo del jugador en el que fue dropeado (Ver si durante drop over poner el border bottom rojo por ej)
-
-  -renderizo de de nuevo la lista
-
-*/
-
-let playerGaps = document.querySelectorAll('.player-gap')
-
-playerGaps.forEach(gap=>{
-  gap.addEventListener('dragover',dragOverGap);
-})
 
